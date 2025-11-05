d903378fa35f91cfd1753bbec7f8c9baf9e1fe80
GGSHGAHYG <2423106@saec.ac.in>
2025-11-05 11:27:28 +0530

    demo

---
+++ src/Dashboard.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import picture from './Images/images.webp'
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+// import Slider from "react-slick";
+
+function Dashboard() {
+
+  //   var settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1
+  // };
+
+  // return (
+  //   <Slider {...settings}>
+  //     <div>
+  //       <h3>1</h3>
+  //     </div>
+  //     <div>
+  //       <h3>2</h3>
+  //     </div>
+  //     <div>
+  //       <h3>3</h3>
+  //     </div>
+  //     <div>
+  //       <h3>4</h3>
+  //     </div>
+  //     <div>
+  //       <h3>5</h3>
+  //     </div>
+  //     <div>
+  //       <h3>6</h3>
+  //     </div>
+  //   </Slider>
+  // );
+    <div> 
+      <h1>hello thi is my project</h1>
+    <div>
+      <img src = {picture} alt='thalapathy'/>
+    </div>
+    </div>
+}
+
+export default Dashboard;
+
+
+
---
+++ src/reportWebVitals.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function reportWebVitals() {
+  return (
+    <div>my login page</div>
+  )
+}
+
+export default reportWebVitals
